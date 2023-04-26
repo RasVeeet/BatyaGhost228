63ac98df4c9d75d26ae48b94e9fd86dc21881c11
IT-Cube <M.Zhilenkoff@yandex.ru>
2023-04-26 18:09:13 +0300

    вонючий фильтр

---
+++ Echo-mod.py
@@ -0,0 +1,41 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram import F
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+API_TOKEN: str = '6231595283:AAFg23Zeg8gHZbRRjNt1zgmJBI7RLZDXs0U'
+
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Хочу кушать')
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+dp.message.register(send_echo)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
